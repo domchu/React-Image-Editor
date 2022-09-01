515d392fff5bd7b0adee993b209d65ac67ca3e12
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-02 00:10:58 +0100

    feature:React icons to the project

--- src/RotateFlip.jsx
+++ src/RotateFlip.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { HiArrowUturnDown } from "react-icons/hi";
 
 const RotateFlip = () => {
   return (
     <div className="image__container">
       <h2>Rotate & Flip</h2>
+      <div className="rotate__icons">
+        <HiArrowUturnDown />
+      </div>
     </div>
   );
 };
