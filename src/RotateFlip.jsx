e000b78c8baaf6ee8c7e13b912c64032432a4f13
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-02 00:29:31 +0100

    chore:container adjustment

--- src/RotateFlip.jsx
+++ src/RotateFlip.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { HiArrowUturnDown } from "react-icons/hi";
+// import { HiArrowUturnDown } from "react-icons/hi";
+import { BsArrowClockwise } from "react-icons/bs";
 
 const RotateFlip = () => {
   return (
     <div className="image__container">
       <h2>Rotate & Flip</h2>
       <div className="rotate__icons">
-        <HiArrowUturnDown />
+        {/* <HiArrowUturnDown /> */}
+        <BsArrowClockwise className="icons" />
       </div>
     </div>
   );
