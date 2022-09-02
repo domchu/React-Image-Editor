038ed4951daa932de01905c35eb6c6c2dbd3db9e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-02 11:47:20 +0100

    chore: added the blur button

--- src/Button.jsx
+++ src/Button.jsx
@@ -6,11 +6,13 @@ import SliderRange from "./SliderRange";
 const Button = () => {
   return (
     <div>
+      <h1>Filter</h1>
       <div className="btn">
         <ButtonDetail name="brightness" />
         <ButtonDetail name="Saturation" />
         <ButtonDetail name="Inversion" />
         <ButtonDetail name="grayscale" />
+        <ButtonDetail name="Blur" />
       </div>
       <SliderRange />
       {/* <RotateFlip /> */}
