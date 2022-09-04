49879602042faab928bf6bed918396fe5dd5915b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-04 01:18:36 +0100

    bug: fix default option

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import "./App.css";
 import Button from "./Button";
 import ButtonDetail from "./ButtonDetail";
-import FilterProps from "./FilterProps";
+// import DEFAULT_OPTIONS from "./DefaultOptions";
+// import FilterProps from "./FilterProps";
 
 function App() {
-  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  const [options, setOptions] = useState("");
   return (
     <div className="page_container">
       <h1>Image Editor</h1>
